161f7a76d4edf6609f0787fb95cad22000be82bc
PedroV <pedrovalerio@ua.pt>
2020-05-12 17:36:38 +0100

    changed notification icon and title

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:deti_motica_app/page/list_page.dart';
 import 'package:deti_motica_app/page/login.dart';
 //import 'package:deti_motica_app/push_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -12,7 +13,7 @@ Future<dynamic> onBackgroundMessageHandler(Map<String, dynamic> message) async {
 }
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  /*WidgetsFlutterBinding.ensureInitialized();
 
   final FirebaseMessaging _fcm=FirebaseMessaging();
   FlutterLocalNotificationsPlugin _lnp=FlutterLocalNotificationsPlugin();
@@ -73,7 +74,7 @@ void main() async {
   _fcm.getToken().then((String token) {
     assert(token != null);
     print(token);
-  });
+  });*/
 
   runApp(DETIMotica());
 }
@@ -103,6 +104,74 @@ class Start extends StatefulWidget {
 }
 
 class _StartState extends State<Start> {
+
+  final FirebaseMessaging _fcm=FirebaseMessaging();
+  FlutterLocalNotificationsPlugin _lnp=FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future onSelectNotification(String payload) async {
+      if (payload != null) {
+        debugPrint('notification payload: ' + payload);
+      }
+    }
+    Future onDidReceiveLocalNotification(int id, String title, String body, String payload) async {
+
+    }
+    Future displayNotification(Map<String, dynamic> message) async{
+      var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+          'channelid', 'flutterfcm', 'your channel description',
+          icon: "@drawable/logo",
+          importance: Importance.Max, priority: Priority.High);
+      var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+      var platformChannelSpecifics = new NotificationDetails(
+          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+      await _lnp.show(
+        0,
+        message['notification']['title'],
+        message['notification']['body'],
+        platformChannelSpecifics,
+        payload: 'hello',);
+    }
+
+    var initializationSettingsAndroid=AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS=IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var initializationSettings=InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    _lnp.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+
+    _fcm.configure(
+      onBackgroundMessage: Platform.isIOS?null:onBackgroundMessageHandler,
+      onMessage: (Map<String, dynamic> message) async {
+        print('on message $message');
+        displayNotification(message);
+        return;
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('on resume $message');
+        return;
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
+        return;
+      },
+    );
+    if (Platform.isIOS) _fcm.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true)
+    );
+    _fcm.onIosSettingsRegistered.listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _fcm.getToken().then((String token) {
+      assert(token != null);
+      print(token);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
